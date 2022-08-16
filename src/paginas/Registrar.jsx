66abbf3e350d7d5ff50c52fd4ff77e3d79b9ecce
Mauro Soares <mauro@tempocerto.inf.br>
2022-08-16 12:12:04 -0300

    :construction:

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,6 +1,22 @@
+
+import './css/Register.css'
+
 function Registrar() {
     return (
-        <div>REGISTER PAGE</div>
+        <div className="input-group input-group-lg">
+            <div></div>
+            <input type="text" placeholder="Número da amostra na palinoteca"/>
+            <input type="text" placeholder="Herbário"/>
+            <input type="text" placeholder="Número de registro da espécie"/>
+            <input type="text" placeholder="Data de coleta"/>
+            <input type="text" placeholder="País"/>
+            <input type="text" placeholder="Estado"/>
+            <input type="text" placeholder="Município"/>
+            <input type="text" placeholder="Tipo de formação vegetal"/>
+            <input type="text" placeholder="Tipo de formação vegetal"/>
+            <input type="text" placeholder="Decimal latitude"/>
+            <input type="text" placeholder="Decimal longitude"/>
+        </div>
     )
 }
 
